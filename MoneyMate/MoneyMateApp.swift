8b03a81b9cc4ec1fcf8daca9d9a90f0c1ef7140b
ppppphrt <phavida.r@ku.th>
2025-04-05 17:03:53 +0700

    Initial Commit

---
+++ MoneyMate/MoneyMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMateApp.swift
+//  MoneyMate
+//
+//  Created by ppppphrt on 5/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
