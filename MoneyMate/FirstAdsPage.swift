9fcffcee6fb69ee3e6effd1f49cef78acf1fa866
ppppphrt <phavida.r@ku.th>
2025-04-05 17:36:49 +0700

    Done first 2 page

---
+++ MoneyMate/FirstAdsPage.swift
@@ -0,0 +1,18 @@
+//
+//  FirstAdsPage.swift
+//  MoneyMate
+//
+//  Created by ppppphrt on 5/4/2568 BE.
+//
+
+import SwiftUI
+
+struct FirstAdsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstAdsPage()
+}
