a63ea4e65f144cf45bf9822aae2c94768c8d4c31
ppppphrt <phavida.r@ku.th>
2025-04-05 17:44:12 +0700

    Done 4 pages

---
+++ MoneyMate/ThirdAdsPage.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdAdsPage.swift
+//  MoneyMate
+//
+//  Created by ppppphrt on 5/4/2568 BE.
+//
+
+import SwiftUI
+
+struct ThirdAdsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThirdAdsPage()
+}
