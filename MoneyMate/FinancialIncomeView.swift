18f453f19a1f1d8757bf5d638410171d0e0503ef
ppppphrt <phavida.r@ku.th>
2025-05-11 15:05:11 +0700

    Added 3 pages with BUGS

---
+++ MoneyMate/FinancialIncomeView.swift
@@ -0,0 +1,18 @@
+//
+//  FinancialIncomeView.swift
+//  MoneyMate
+//
+//  Created by ppppphrt on 11/5/2568 BE.
+//
+
+import SwiftUI
+
+struct FinancialIncomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinancialIncomeView()
+}
---
+++ MoneyMate/IncomeTrackerView.swift
@@ -0,0 +1,18 @@
+//
+//  IncomeTrackerView.swift
+//  MoneyMate
+//
+//  Created by ppppphrt on 11/5/2568 BE.
+//
+
+import SwiftUI
+
+struct IncomeTrackerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IncomeTrackerView()
+}
