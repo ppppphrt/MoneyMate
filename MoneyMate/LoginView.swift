3ccf78dfff8e67a2c3a9fb3bd4ccb8e0ba62c6d5
ppppphrt <phavida.r@ku.th>
2025-05-11 15:35:12 +0700

    Login and Register Worked

---
+++ MoneyMate/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  MoneyMate
+//
+//  Created by ppppphrt on 11/5/2568 BE.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
